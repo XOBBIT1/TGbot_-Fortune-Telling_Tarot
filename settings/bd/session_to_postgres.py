666cd96cb7072aa6f8a7e006a9e46fb3502b2c58
Денис Гришанков <ben300300@gmail.com>
2023-05-16 17:14:35 +0300

    deleted some changes

--- settings/bd/session_to_postgres.py
+++ settings/bd/session_to_postgres.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 def create_dbsession(**kwargs):
-    db_path = config_settings.db_url
+    db_path = f'postgresql://{config_settings.user}:{config_settings.password}@{config_settings.host}:{config_settings.port}/{config_settings.db_name}'
     engine = db.create_engine(db_path)
     SessionClass = sessionmaker(bind=engine)
     return SessionClass()
